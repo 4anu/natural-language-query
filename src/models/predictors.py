856328607552993b3ce31f7cf8c465b0c797cf70
Anand Mohan <moghan.anand@gmail.com>
2018-04-12 19:27:40 +0530

    Without CNN Test.

--- src/models/predictors.py
+++ src/models/predictors.py
@@ -22,10 +22,10 @@ class AggregatePredictor(nn.Module):
         self.embedding_layer = embedding_layer
 
         self.dropout = nn.Dropout(const.AGG_CNN_DROPOUT)
-        self.cnn_layer = nn.Conv2d(in_channels=1, out_channels=const.AGG_CNN_NUM_FILTERS,
+        '''self.cnn_layer = nn.Conv2d(in_channels=1, out_channels=const.AGG_CNN_NUM_FILTERS,
                                    kernel_size=const.AGG_CNN_KERNEL_SIZE, stride=const.AGG_CNN_STRIDE,
                                    padding=(int(const.AGG_CNN_KERNEL_SIZE[0] / 2), 0))
-
+        '''
         self.rnn_layer = nn.GRU(args.emb_size, hidden_size=const.AGG_RNN_SIZE, num_layers=const.AGG_RNN_LAYERS,
                                 dropout=const.AGG_RNN_DROPOUT)
         self.rnn_hidden_state = init_hidden(const.AGG_RNN_LAYERS, args.batch_size, const.AGG_RNN_SIZE, args.gpu)
@@ -42,10 +42,11 @@ class AggregatePredictor(nn.Module):
         input_embeddings = self.embedding_layer.get_embedding(input)
 
         # cnn_input = self.dropout(input_embeddings).unsqueeze(1)
-        cnn_input = input_embeddings.unsqueeze(1)
+        '''cnn_input = input_embeddings.unsqueeze(1)
         cnn_output = self.cnn_layer(cnn_input)
         cnn_output = funct.relu(cnn_output).squeeze(3)
-        cnn_output = torch.transpose(cnn_output, 1, 2)
+        cnn_output = torch.transpose(cnn_output, 1, 2)'''
+        cnn_output = input_embeddings
         cnn_output = torch.transpose(cnn_output, 0, 1)
 
         rnn_output, self.rnn_hidden_state = self.rnn_layer(cnn_output, self.rnn_hidden_state)
@@ -54,8 +55,8 @@ class AggregatePredictor(nn.Module):
         rnn_output = funct.max_pool1d(rnn_output, rnn_output.size(2)).squeeze(2)
 
         dense_layer_1_output = self.dense_layer_1(funct.sigmoid(rnn_output))
-        net_out = funct.sigmoid(dense_layer_1_output)
         # dense_layer_2_output = self.dense_layer_2(funct.sigmoid(dense_layer_1_output))
+        net_out = funct.sigmoid(dense_layer_1_output)
         return net_out
 
 
